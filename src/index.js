d715d0928d8984656b690510b165d8dd8fa05669
aranida14 <dinara1602@gmail.com>
2023-11-30 01:20:09 +0300

    loadPage implementation v1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const loadPage = (url, outputDir = process.cwd()) => (
-  Promise.resolve(`loadPage (${url}, ${outputDir}) says Hi`)
-);
+import * as path from 'node:path';
+import * as fs from 'node:fs/promises';
+import axios from 'axios';
+
+const loadPage = (url, outputDir = process.cwd()) => {
+  // console.log(outputDir);
+  const pageUrl = new URL(url);
+  const { protocol } = pageUrl;
+  const nameWithoutProtocol = pageUrl.href.replace(`${protocol}//`, '');
+  const filename = `${nameWithoutProtocol.replace(/[^0-9a-zA-Z]/g, '-')}.html`;
+  const filepath = path.join(outputDir, filename);
+  return axios({
+    method: 'get',
+    url,
+    responseType: 'text',
+  })
+    .then((response) => fs.writeFile(filepath, response.data))
+    .then(() => filepath);
+};
 
 export default loadPage;
